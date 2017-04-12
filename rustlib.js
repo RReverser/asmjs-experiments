30fc700089c5471f122a3bae118ea2fd2d7ae64f
Ingvar Stepanyan <me@rreverser.com>
2017-04-12 22:59:08 +0100

    Use Pointer_stringify to decode Rust strings

--- rustlib.js
+++ rustlib.js
@@ -1,14 +1,13 @@
 mergeInto(LibraryManager.library, {
-  _embind_register_rust_string__deps: ['$readLatin1String', '$registerType'],
-  _embind_register_rust_string: function(rawType, name) {
-    name = readLatin1String(name);
+  _embind_register_rust_string__deps: ['$registerType'],
+  _embind_register_rust_string: function(rawType) {
     registerType(rawType, {
-        name: name,
+        name: "&'static str",
         'argPackAdvance': 8,
         'readValueFromPointer': function (pointer) {
             var length = HEAPU32[(pointer >> 2) + 1];
             pointer = HEAPU32[pointer >> 2];
-            return new TextDecoder().decode(HEAPU8.subarray(pointer, pointer + length));
+            return Pointer_stringify(pointer, length);
         }
     });
   }
