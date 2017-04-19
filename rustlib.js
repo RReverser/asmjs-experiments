12e3546e6b741d3767201ec5c10a78a74b74e169
Ingvar Stepanyan <me@rreverser.com>
2017-04-20 00:29:56 +0100

    Update rustlib.js too

--- rustlib.js
+++ rustlib.js
@@ -2,7 +2,7 @@ mergeInto(LibraryManager.library, {
   _embind_register_rust_string__deps: ['$registerType'],
   _embind_register_rust_string: function(rawType) {
     registerType(rawType, {
-        name: "&'static str",
+        name: "&str",
         'argPackAdvance': 8,
         'readValueFromPointer': function (pointer) {
             var length = HEAPU32[(pointer >> 2) + 1];
